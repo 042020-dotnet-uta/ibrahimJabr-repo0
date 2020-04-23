6343bdb7e5872a21bc869873d87f6ab109cc18cd
Ibrahim Jabr <ibrahim.aljabr93@gmail.com>
2020-04-23 09:46:16 -0400

    same game but OOP using classes

---
+++ RPS_Game/RPS_Game/Game.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+
+namespace RPS_Game
+{
+    public class Game
+    {
+        public List<Round> Rounds =new List<Round>(); 
+        private Player p1;
+        public Player P1
+        {
+            get{return p1;}
+            set{p1 = value;}
+        }
+        private Player p2;
+        public Player P2
+        {
+            get{return p2;}
+            set{p2 = value;}
+        }
+
+        private Player winner;
+        public Player Winner
+        {
+            get{return winner;}
+            set{winner = value;}
+        }
+        private int roundNumber;
+        public int RoundNumber
+        {
+            get{return roundNumber;}
+            set{roundNumber = value;}
+        }
+        public void playAGame(Player P1,Player P2){
+            Round round = null;
+            while (P1.Wins != 2 && P2.Wins != 2)
+            { // using while loop to run the rounds until one player reaches a score of 2.
+             round = new Round();
+                round.P1Choice = round.RPSGenerator(); // generate r/p/s to store into playerCurntround.
+                round.P2Choice = round.RPSGenerator();
+                if (round.P1Choice == "rock")
+                { // conditionals to check who won each round. If player one has a "rock"
+                    if (round.P2Choice == "paper")
+                    { // nested if statements to compare different result P2 will have with P1 having "rock"
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose rock, {P2.Name} chose paper. - P2 won"); // adds winner of the round to the array.
+                        P2.Wins++; // adds a win count to player 2
+                        P1.Losses++;
+                    }
+                    else if (round.P2Choice == "scissor")
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose rock, {P2.Name} chose scissor. - P1 won");
+                        P1.Wins++;
+                        P2.Losses++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose rock, {P2.Name} chose rock. - it is a ties");
+                        P1.Ties++; // adds a tie count 
+                        P2.Ties++;
+                    }
+                }
+                else if (round.P1Choice == "paper")
+                {
+                    if (round.P2Choice == "rock")
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose paper, {P2.Name} chose rock. - P1 won");
+                        P1.Wins++;
+                        P2.Losses++;
+                    }
+                    else if (round.P2Choice == "scissor")
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose paper, {P2.Name} chose scissor. - P2 won");
+                        P2.Wins++;
+                        P1.Losses++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose paper, {P2.Name} chose paper. - it is a ties");
+                        P1.Ties++; // adds a tie count 
+                        P2.Ties++;
+                    }
+                }
+                else
+                {
+                    if (round.P2Choice == "rock")
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose scissor, {P2.Name} chose paper. - P1 won");
+                        P1.Wins++;
+                        P2.Losses++;
+                    }
+                    else if (round.P2Choice == "scissor")
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose scissor, {P2.Name} chose rock. - P2 won");
+                        P2.Wins++;
+                        P1.Losses++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Round {RoundNumber} - {P1.Name} chose scissor, {P2.Name} chose scissor. - it is a ties");
+                        P1.Ties++; // adds a tie count 
+                        P2.Ties++;
+                    }
+                }
+                RoundNumber++; // updates the round number each loop.
+            }
+        }
+    }
+}
---
+++ RPS_Game/RPS_Game/Round.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+
+namespace RPS_Game
+{
+    public class Round
+    {
+        private Player winnner;
+        public Player Winnner
+        {
+            get{return winnner;}
+            set{winnner =value;}
+        }
+        private string p1Choice;
+        public string P1Choice
+        {
+            get{return p1Choice;}
+            set{p1Choice =value;}
+        }
+        private string p2Choice;
+        public string P2Choice
+        {
+            get{return p2Choice;}
+            set{p2Choice =value;}
+        }
+        public Round(){}
+        public string RPSGenerator()
+        { // method to generate rock, paper, or scissor for each player.
+            Random rnd = new Random(); // object class created to generate random number.
+            int rundomNum = rnd.Next(3); // generate random number from 0 - 2.
+            switch (rundomNum)
+            { // returning rock, paper, or scissor based off of what random number you generate.
+                case 0:
+                    return "rock";
+
+                case 1:
+                    return "paper";
+
+                case 2:
+                    return "scissor";
+                default:
+                    return "Default case";
+            }
+        }
+    }
+}
